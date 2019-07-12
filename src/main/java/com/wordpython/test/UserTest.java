eb1e0470abdcfb17cb445bfd13955ee48402e361
wordpython <wordpython@163.com>
2019-07-12 12:44:00 +0800

    用maven简单整合了hibernate,仅有数据插入，测试成功

---
+++ src/main/java/com/wordpython/test/UserTest.java
@@ -0,0 +1,38 @@
+package com.wordpython.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import com.wordpython.domain.User;
+
+public class UserTest {
+	@Test
+	public void saveUser() {
+		//保存用户数据
+		//1.获取核心配置文件对象，默认是加载src的hibernate.cfg.xml文件
+//		Configuration cfg=new Configuration().configure();
+		Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
+		//2.创建会话工厂
+		SessionFactory factory=cfg.buildSessionFactory();
+		//3.创建会话（相当于连接Connect）
+		Session session=factory.openSession();
+		//开启事务
+		Transaction trans=session.beginTransaction();
+		//保存
+		User user=new User();
+		user.setAccount("111111111");
+		user.setUsername("1aaaa2");
+		user.setPassword("cccccc");
+		user.setPhone("98765432123");
+		session.save(user);
+		//提交事务
+		trans.commit();
+		//4.关闭会话
+		session.close();
+		//5.关闭工厂，释放资源
+		factory.close();
+	}
+}
